57ac81883fc5dbae67937043b44456cc5b27fb81
Tim Menzies <timm@users.noreply.github.com>
2023-12-22 15:34:23 -0500

    saving

---
+++ get.lisp
@@ -0,0 +1,12 @@
+#+clisp
+(defun loader (x) (load x))
+
+#+sbcl
+(defun loader (x)
+  (handler-bind
+      ((simple-warning 
+        #'(lambda (w) 
+            (when (undefined-variable-warning-p w)
+              (invoke-restart 'muffle-warning)))))
+      (load x))
+    (load x))
