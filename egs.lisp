008fb65c90280270aa6f847f7b02acf0adc84452
Tim Menzies <timm@ieee.org>
2024-02-17 00:40:25 -0500

    as

--- egs.lisp
+++ egs.lisp
@@ -1,8 +1,9 @@
 (defun egs ()
-  (labels ((eg (x) (equalp "eg-" (subseq (format nil "~(~a~)   " x) 0 3))))
-    (loop :for x :being :the symbols :in *package* :if (eg (symbol-name x)) :collect x)))
+  (labels ((eg (x) (equal "eg-" (subseq (format nil "~(~a~)  " (symbol-name x)) 0 3))))
+    (loop :for x :being :the symbols :in *package* :if (eg x) :collect x)))
 
 (defun eg-a(x) x)
 (defun eg-b(x) x)
 
+(defun it(a) a)
 (print (egs))
