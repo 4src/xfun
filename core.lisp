87697550fecd56b4a2b5b341c2d1fd83990767e2
timm <timm@ieee.org>
2024-07-09 00:39:50 -0400

    as

--- core.lisp
+++ core.lisp
@@ -1,6 +1,7 @@
 ; vim : set ts=2 sw=2 sts=2
-(set-macro-character #\$ #'(lambda (s _) `(slot-value self ',(read s t nil t))))
-(set-macro-character #\! #'(lambda (s _) `(o the ,(read s t nil t))))
+(set-macro-character #\$ #'(lambda (s _)
+                             (declare (ignore _))
+                             `(slot-value self ',(read s t nil t))))
 
 (defmacro o (struct f &rest fs)
   (if fs `(o (slot-value ,struct ',f) ,@fs) `(slot-value ,struct ',f)))
@@ -8,9 +9,9 @@
 (defmacro aif (test yes &optional no) 
   `(let ((it ,test)) (if it ,yes ,no)))
 
-(defmacro inca (x lst &optional (init 0))
-  `(incf (cdr (or (assoc ,x ,lst :test #'equal) 
-                  (car (setf ,lst (cons (cons ,x ,init) ,lst)))))))
+(defmacro has (x lst &optional (init 0))
+  `(cdr (or (assoc ,x ,lst :test #'equal) 
+            (car (setf ,lst (cons (cons ,x ,init) ,lst))))))
 ;---------- --------- --------- --------- --------- --------- --------- ---------
 (defstruct stats (boostraps 1234567891) (cohen 0.35))
 
@@ -36,18 +37,76 @@
           (if there (split s (1+ there))))))
 
 (defun with-csv (file &optional (fun #'print) (filter #'split))
-  (with-open-file (s (or file  *standard-input*))
-    (loop (funcall fun (funcall filter (or (read-line s nil) (return)))))))
+  (with-open-file (s (or file *standard-input*))
+    (loop (funcall fun (funcall filter (or (read-line s nil) (return file)))))))
 ;---------- --------- --------- --------- --------- --------- --------- ---------
-(defstruct sym (pos 0) (txt " ") (n 0)  has mode (most 0))
+(defstruct sym (pos 0) (txt " ") (n 0)  seen mode (most 0))
 (defstruct num (pos 0) (txt " ") (n 0) (lo 1e30) (hi -1e30) (mu 0) (m2 0) (goal 1))
 
-(defun makeNum (&key (at 0) (txt " "))
-  (make-num :at at :txt txt  :goal (if (eq #\- (last-char txt)) 0 1)))
+(defun makeNum (&key txt (pos 0))
+  (make-num :pos pos :txt txt :goal (if (eq #\- (last-char txt)) 0 1)))
+
+(defmethod add ((self num) x)
+  (unless (eq #\? x)
+    (incf $n)
+    (let ((d (- x $mu)))
+      (incf $mu  (/ d $n))
+      (incf $m2  (* d (-  x $mu)))
+      (setf $lo  (min x $lo)
+            $hi  (max x $hi)))))
+
+(defmethod add ((self sym) x)
+  (unless (eq x '?)
+    (incf $n)
+    (let ((new (incf (has x $seen))))
+      (if (> new $most)
+          (setf $mode x 
+                $most new)))))
+
+(defmethod mid ((self num)) $mu)
+(defmethod mid ((self sym)) $mode)
+
+(defmethod div ((self num)) (if (< $n 2) 0 (sqrt (/ $m2 (- $n 1)))))
+(defmethod div ((self sym))
+  (* -1 (loop :for (_ . v) :in $has :sum  (* (/ v $n) (log (/ v $n) 2)))))
+;---------- --------- --------- --------- --------- --------- --------- ---------
+(defstruct data rows cols)
+(defstruct cols all x y names)
+
+(defmethod clone ((self data) &optional inits)
+  (slurp (make-data) (cons (data-cols (cols-name self)) inits)))
+  
+(defmethod slurp ((self data) (file string))
+  (with-csv file (lambda (row) (add self row))))
+
+(defmethod slurp ((self data) (lst cons))
+    (dolist (row lst self) (add self row)))
+
+(defmethod add ((self data) row)
+  (if cols 
+      (push (add cols row) $rows) 
+      (setf $cols (make-cols (row-cells row1)))))
+      
+(defun make-cols (names)   
+  (let (all x y (n -1))
+    (dolist (col1 (loop :for s :in names :collect (make-col :at (incf n) :txt s)) 
+                  (%make-cols :names names :all all :x x :y y))
+      (push col1 all)
+      (when (not (eq #\X (last-char (o col1 txt))))
+        (if (member (last-char (o col1 txt)) '(#\+ #\-))
+          (push col1 y)
+          (push col1 x))))))
+
+(defmethod add ((cols1 cols) (row1 row))
+  (with-slots (x y) cols1
+    (dolist (tmp (list x y) row1)
+      (dolist (col tmp) 
+        (add col (elt (row-cells row1) (o col at)))))))
+
 ;---------- --------- --------- --------- --------- --------- --------- ---------
 (defun egs()
   (labels ((down (x) (string-downcase (symbol-name x)))
-           (eg (s) (equalp "eg-" (subseq s 0 (min 3 (length s))))))
+           (eg   (s) (equalp "eg-" (subseq s 0 (min 3 (length s))))))
     (loop :for x :being :the symbols :in *package* :if (eg (down x)) :collect x)))
 
 (defun eg-o () (o our stats cohen))
