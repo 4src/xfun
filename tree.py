e60a4e889a285bbd92a0c26bd3f3495f0cc34e67
timm <timm@ieee.org>
2024-07-23 15:00:15 -0400

    saving

--- tree.py
+++ tree.py
@@ -3,7 +3,7 @@
 from fileinput import FileInput as file_or_stdin
 import re,ast,sys,random
 from math import log,floor
-any=random.chice
+any=random.choice
 
 class o:
   __init__ = lambda i,**d : i.__dict__.update(d)
@@ -12,16 +12,16 @@ class o:
 the = o(train="data/misc/config")
 
 the = o(
-  seed  = 1234567891, 
+  seed  = 1234567891,
   round = 2,
-  train = "data/misc/auto93.csv",  
+  train = "data/misc/auto93.csv",
   dist  = o(p=2,
-            fars=30,
-            stop=10))                                                   
-            
+            far=30,
+            stop=10))
+
 class COL(o):
   def __init__(i,txt=" ",at=0): i.n,i.txt, i.at = 0, txt, at
-  def add(i,x)  : pass 
+  def add(i,x)  : pass
   def norm(i,x) : return x
 
 class SYM(COL):
@@ -49,84 +49,79 @@ class NUM(COL):
   def div(i)    : return 0 if i.n < 2 else (i.m2/(i.n - 1))**.5
   def mid(i)    : return i.mu
   def norm(i,x) : return x if x=="?" else (x - i.lo)/(i.hi - i.lo + 1E-32)
-  
+
   def dist(i,x,y):
     if x==y=="?": return 1
     x, y = i.norm(x), i.norm(y)
     x = x if x !="?" else (1 if y<0.5 else 0)
     y = y if y !="?" else (1 if x<0.5 else 0)
-    return abs(x-y)
+    return abs(x-y)
 
 class DATA(o):
   def __init__(i)      : i.rows=[];  i.cols = o(all=[],x=[],y=[],names=[])
   def add(i,row)       : (i.data if i.cols.all else i.head)(row)
   def clone(i,rows=[]) : return DATA().fromList([i.cols.names] + rows)
   def fromFile(i,file) : [i.add(row) for row in csv(file)]; return i
-  def fromList(i,lst)  : [i.add(row) for row in lst      ]; return i 
+  def fromList(i,lst)  : [i.add(row) for row in lst      ]; return i
   def sort(i)          : i.rows.sort(key = i.chebyshev)   ; return i
 
-  def data(i,row): 
+  def data(i,row):
     [col.add(x) for col,x in zip(i.cols.all,row) if x != "?"]
     i.rows += [row]
 
-  def head(i,row): 
+  def head(i,row):
     i.cols.names = row
     for at,txt in enumerate(row):
-      col = (NUM if txt[0].isupper() else SYM)(txt=txt,at=at)  
+      col = (NUM if txt[0].isupper() else SYM)(txt=txt,at=at)
       i.cols.all.append(col)
       if txt[-1] != "X":
         (i.cols.y if col.txt[-1] in "+-!" else i.cols.x).append(col)
-        
-  def dists(i, row1, row2): 
-    n = sum(dist(col, row1[col.at], row2[col.at])**the.dist.p for col in i.cols.x)
+
+  def dists(i, row1, row2):
+    n = sum(col.dict(row1[col.at], row2[col.at])**the.dist.p for col in i.cols.x)
     return (n / len(i.cols.x))**(1/the.dist.p)
 
   def neighbors(i, row1, rows):
-    return sorted(rows or o.rows, key=lambda row2: i.dists(,row1,row2))
+    return sorted(rows or o.rows, key=lambda row2: i.dists(row1,row2))
 
-  def twoFar(i, rows) : 
-    return most((any(rows),any(rows)) for _ in range(the.dists.far),
+  def twoFar(i, rows) :
+    return max(((any(rows),any(rows)) for _ in range(the.dists.far)),
                 key= lambda two: i.dists(*two))
-  
+
   def half(i,rows):
-    left,right = i.twoFar(rows) 
+    left,right = i.twoFar(rows)
     toLeft = i.dist(left,right)/2
     lefts,rights = [],[]
     for i,row in rows:
       (lefts if i.dists(row,left) <= toLeft else rights).append(row)
-    return left, right, lefts, rights, toLeft
+    return lefts, rights, left, right, toLeft
 
-  def cluster(i, rows, lvl=0, gaurd=nil):  
-    node = TREE(i.clone(rows), lvl, gaurd)
-    it.left, it.right, ls, rs, it.toLeft = i.half(rows)
-    go   = lambda row: i.dists(row,left) <= it.toleft
-    nogo = lambda row: not go(row)
-    if node.ok2go(ls): it.lefts  = i.cluster(ls, lvl+1, gaurd = go)
-    if node.ok2go(rs): it.rights = i.cluster(rs, lvl+1, gaurd = nogo)
-    return node
+  def cluster(i, rows, lvl=0, guard=None):
+    it = TREE(i.clone(rows), lvl, guard)
+    ls, rs, it.left, it.right, it.toLeft = i.half(rows)
+    go = lambda row: i.dists(row,it.left) <= it.toleft 
+    if it.ok2go(ls): it.lefts  = i.cluster(ls, lvl+1, guard = go)
+    if it.ok2go(rs): it.rights = i.cluster(rs, lvl+1, guard = lambda row: not go(row))
+    return it
 
 class TREE(o):
-  def __init__(i, here,lvl,gaurd): i.here, i.lvl, i.gaurd = here, lvl, gaurd
-  def ok2go(i,rows)  : return len(rows) > the.dist.stop and len(rows) < len(i.here.rows) 
-  def go(i,row)      : return i.gaurd(row)
-  def nogo(i,row)    : return not i.gaurd(row)
+  def __init__(i, here,lvl,guard): i.here, i.lvl, i.guard = here, lvl, guard
+  def ok2go(i,rows)  : return len(rows) > the.dist.stop and len(rows) < len(i.here.rows)
+  def go(i,row)      : return i.guard(row)
+  def nogo(i,row)    : return not i.guard(row)
 
   def leaf(i,row) :
     for kid in [i.lefts,i.rights]:
-      if kid.go(row): return kid.leaf(row)
+      if kid.guard(row): return kid.leaf(row)
     return i
 
   def predict(i,row):
-    here         = i.leaf(row).here
-    row1,row2,_* = here.neighbors(row)
-    d1,d2        = here.dist(row1,row), data,dist(row2,row)
-    w1,w2        = 1/d1**2, i/d2**2
-    out={}
-    for col in data.cols.y do
-      y1, y2      = row1[col.at], row2[col.at]
-      out{col.at} = (y1 * w1) + (y2 * w2) / (w1 + w2)
-    return out
-    
+    here     = i.leaf(row).here
+    r1,r2,*_ = here.neighbors(row)
+    d1,d2    = here.dists(r1,row), here.dists(r2,row)
+    w1,w2    = 1/d1**2, 1/d2**2 
+    return {c.at: ((r1[c.at] * w1) + (r2[c.at] * w2) / (w1 + w2)) for c in here.cols.y} 
+
 def coerce(s):
   try: return ast.literal_eval(s)
   except Exception:  return s
@@ -135,4 +130,4 @@ def csv(file="-"):
   with file_or_stdin(None if file=="-" else file) as src:
     for line in src:
       line = re.sub(r'([\n\t\r ]|#.*)', '', line)
-      if line: yield [coerce(s.strip()) for s in line.split(",")]  
+      if line: yield [coerce(s.strip()) for s in line.split(",")]
