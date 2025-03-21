8232a2506d8a7ab3cb2a7fa3fb3f043cd5316a37
timm <timm@ieee.org>
2025-03-20 21:18:25 -0400

    as

--- lets.lisp
+++ lets.lisp
@@ -1,19 +1,51 @@
-(defmacro lets (bindings &body body)
-  "Combined let*, labels, and multiple-value-bind with flexible ordering"
-  (let ((out `(progn ,@body)))
-    (dolist (b (reverse bindings) out)
-      (setf out (cond ((and (listp b) (> (length b) 2) (symbolp (car b)) (listp (cadr b)))
-                       `(labels ((,(car b) ,(cadr b) ,@(cddr b))) ,out))
-                      ((and (listp b) (listp (car b)))
-                       `(multiple-value-bing ,(car b) ,(cadr b) ,out))
-                      (t `(let* (,b) ,out)))))))
+;Here's one I've dreamt of  for a while: a LISP variant of "let" that handles let*, labels, multiple-value-bind. Built with http://perplexiity.ai. FYI it required much wrangling and testing, but the key expansion tactic seen  at end of "lets" was all its idea (impressive).
+;(defmacro lets (bindings &body body)
+;  "Combined let*, labels, and multiple-value-bind with flexible ordering"
+;  (labels ((fun (x b4)
+;                (cond ((and (listp x) (> (length x) 2) (symbolp (car x)) (listp (cadr x)))
+;                       `(labels ((,(car x) ,(cadr x) ,@(cddr x))) ,b4))
+;                      ((and (listp x) (listp (car x)))
+;                       `(multiple-value-bind ,(car x) ,(cadr x) ,b4))
+;                      (t `(let* (,x) ,b4)))))
+;    (reduce #'fun bindings :initial-value `(progn ,@body) :from-end t)))
 
-;; example
+;
+;(defmacro lets (bindings &body body)
+;  (labels ((wrap (bind inner)
+;             (destructuring-bind (type vars form) bind
+;               (ecase type
+;                 (~ `(multiple-value-bind ,vars ,form ,inner))
+;                 (=  `(destructuring-bind ,vars ,form ,inner))
+;                 (!  `(labels ((,@vars ,@form)) ,inner))
+;                 (t  `(let* ((,type ,vars)) ,inner))))))
+;    (reduce #'wrap bindings :initial-value `(progn ,@body) :from-end t)))
+;
+;; exampleo
+
+(defmacro lets (bindings &body body)
+  (labels ((wrap (bind inner)
+             (destructuring-bind (type vars &rest forms) bind
+               (case type
+                 (* `(multiple-value-bind ,vars ,(first forms) ,inner))
+                 (~ `(destructuring-bind ,vars ,(first forms) ,inner))
+                 (! `(labels ((,vars ,@forms)) ,inner))
+                 (= `(let* ((,vars ,(first forms))) ,inner))
+                 (otherwise (error "Unknown binding type: ~S" type))))))
+    (reduce #'wrap (reverse bindings) :initial-value `(progn ,@body))))
+
+
+
 (pprint (macroexpand-1 
-          '(lets (((a b) (c 122))
-                  (_f1 (x) (* x 2))
-                  (c (_f1 21))
-                  ((a b) (c 122))
-                  (_f2 (x) (* x 2))
-                  (c (_f2 21)))
-                 (print a))))
+          ' (lets ((* (a b) (a 1 2))
+                   (~ (c . d) '(3 . 4))
+                   (! f (x) (* x 2))
+                   (= x 10))
+                  (list a b c d (f x)))))
+
+
+;Got any other cool  LISP macros ?  My one  fav is to make "self" the first defmethod arg then use   
+
+(set-macro-character #\$  
+   #'(lambda (s _) `(slot-value self ',(read s t nil t))))  
+
+;so self's slots can be accessed/changed using just $slot
